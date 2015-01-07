b882a102761c1369dec6206081683dfd34ef6521
Oren Leiman <oleiman@neutral.io>
2015-01-07 10:45:59 -0800

    BLK-237 Block tests conform to most recent NIOBlockTestCase

--- tests/test_clone_signals_block.py
+++ tests/test_clone_signals_block.py
@@ -17,7 +17,7 @@ class TestCloneSignals(NIOBlockTestCase):
         super().setUp()
         self.last_notified = []
 
-    def signals_notified(self, signals):
+    def signals_notified(self, signals, output_id='default'):
         self.last_notified = signals
 
     def test_pass(self):
